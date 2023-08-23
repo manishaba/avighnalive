5edb88d08188ccfddf52df6e4473db0be585e525
adeebwajih <adeebwajih@abainfotech.com>
2023-08-23 16:38:48 +0530

    add .htacess file and remove header frim one file

--- assets/js/common.js
+++ assets/js/common.js
@@ -35,4 +35,15 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     }  
+    $('.topics a').on('click', function() {
+        //selecting the syllabus class
+        $select = $('<div class="syllabus"></div>');
+        $(this).parents('li').each(function(n, li) {
+            //Adding / to each anchor tag of li
+            $select.prepend(' / ',$(li).children('a').clone());
+        });
+        // Displaying the hierarchical order of pages.
+        $('.display').html(
+        $select.prepend('<a href="#syllabus">Home</a>'));
+    })
 });
