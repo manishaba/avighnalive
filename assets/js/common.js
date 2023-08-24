0f8a6b3452aba7d489a8d93d3dce3e25e56259e4
adeebwajih <adeebwajih@abainfotech.com>
2023-08-24 17:21:42 +0530

    Breadcrum changes and removed extra code

--- assets/js/common.js
+++ assets/js/common.js
@@ -35,15 +35,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     }  
-    $('.topics a').on('click', function() {
-        //selecting the syllabus class
-        $select = $('<div class="syllabus"></div>');
-        $(this).parents('li').each(function(n, li) {
-            //Adding / to each anchor tag of li
-            $select.prepend(' / ',$(li).children('a').clone());
-        });
-        // Displaying the hierarchical order of pages.
-        $('.display').html(
-        $select.prepend('<a href="#syllabus">Home</a>'));
-    })
+    // $('.topics a').on('click', function() {
+    //     $select = $('<div class="syllabus"></div>');
+    //     $(this).parents('li').each(function(n, li) {
+    //         $select.prepend(' / ',$(li).children('a').clone());
+    //     });
+    //     $('.display').html(
+    //     $select.prepend('<a href="#syllabus">Home</a>'));
+    // })
 });
